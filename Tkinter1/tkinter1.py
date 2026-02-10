7fe5e18f448fab1915a13c7bc62706938fa33bb8
Sudharshini <156495@student.bmg.vic.edu.au>
2026-02-11 10:23:18 +1100

    second commit

--- Tkinter1/tkinter1.py
+++ Tkinter1/tkinter1.py
@@ -1,2 +1,10 @@
-print("hello world")
-print("welcome to python programming")
+import tkinter as tk
+root = tk.Tk()
+root.title("My First App")
+root.geometry("300x200")
+
+label = tk.Label(root, text="Hello, Tkinter!")
+label.place(x = 110, y = 40)
+button = tk.Button(root, text="Click Me", command = lambda:label.config(text = "you clicked the button!"))
+button.place(x = 110, y = 80)
+root.mainloop()
---
+++ Tkinter1/tkinter2.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Hello Tkinter")
+root.geometry("500x500")
+
+
+frame = ttk.Frame(root, padding = 12)
+frame.grid(row=0, column=0, sticky= "NSEW")
+root.rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)   
+frame.columnconfigure(1, weight=1)
+
+name_var = tk.StringVar()
+ttk.Label(frame, text = "Name:").grid(row=0, column=0, sticky="W", pady = 6)
+ttk.Entry(frame, textvariable = name_var).grid(row=0, column=1, sticky="EW", pady = 6)
+root.mainloop()
