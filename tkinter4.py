a4ac60dfb5cdca49a2461f3e4610c08a6de1a868
Sudharshini <156495@student.bmg.vic.edu.au>
2026-02-13 12:12:52 +1100

    3rd

--- tkinter4.py
+++ tkinter4.py
@@ -5,9 +5,10 @@ from tkinter import messagebox
 #set up root and frame
 root = tk.Tk()
 root.title("Student Information")
-root.geometry("500x250")
+root.geometry("950x450")
 root.columnconfigure(0, weight=1)  
 root.rowconfigure(0, weight=1)
+
 
 style = ttk.Style()
 style.configure('pink.TFrame', background='pink')
@@ -28,6 +29,21 @@ ttk.Entry(frame, textvariable = name_var).grid(row=1, column=1, sticky="EW", pad
 
 ttk.Label(frame, text = "Bus Traveller:").grid(row=2, column=0, sticky="W", pady = 6, padx = 6)
 ttk.Checkbutton(frame, text = "True", variable = bus_var).grid(row=2, column=1, sticky="W", pady = 6, padx = 6)
+
+treeview = ttk.Treeview(frame, columns=("Id","Name", "Bus"),show="headings")
+treeview.grid(row=5, column=0, columnspan=2, sticky = "EW",pady = 6)
+treeview.heading("Id", text = "Id")
+treeview.heading("Name", text = "Name")
+treeview.heading("Bus", text = "Bus")
+treeview.column("Id", minwidth = 100, width= 100)
+treeview.column("Name", minwidth = 100, width= 100)
+treeview.column("Bus", minwidth = 100, width= 100)
+
+treeview.insert("", tk.END, values=("123456", "Sudharshini", False))
+treeview.insert("", tk.END, values=("654321", "Bob", False))
+
+style = ttk.Style()
+style.configure("Treeview", rowheight = 30)
 
 def save(_event=None):
     if not id_var.get().strip().isdigit():
