dffdebf5ae1d93048bc6e28aa2cf049f7fa2348d
Sudharshini <156495@bmg.vic.edu.au>
2026-02-26 21:58:10 +1100

    Updated Number

--- Python/Number.py
+++ Python/Number.py
@@ -1,16 +1,22 @@
-number = (input('Enter a number: '))
 count = 0 
 total = 0
 
-while number == number:
+while True:
     number = (input('Enter a number: '))
-    count += 1
-    total += number
-    if number == '': 
+    print('You entered: ', number)
+    if number.isdigit():
+        number = int(number)
+        count += 1
+        total += number
+    else:
+        print('Invalid input')
         break
+    
 
 print('Number of inputs: ', count)
 print('Sum of numbers: ', total)
+print('Average: ', total/count)
+
 
 
 
