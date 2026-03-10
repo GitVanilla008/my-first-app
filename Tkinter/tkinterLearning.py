2e5592f60289b3aaeea18c455d3b05cd4170839c
Sudharshini <156495@bmg.vic.edu.au>
2026-03-10 23:10:55 +1100

    idk

--- Tkinter/tkinterLearning.py
+++ Tkinter/tkinterLearning.py
@@ -1,7 +1,10 @@
 import tkinter as tk
+from tkinter import ttk #treeview is not in tkinter so we have to add it
 
 root = tk.Tk() #this creates the window
-root.title("My First Window") #this sets the title of the window
-root.geometry("400x300") #this sets the size of the window, width x height
+root.title("Student Table") #this sets the title of the window
+root.geometry("500x300") #this sets the size of the window, width x height
+
+tree = ttk.Treeview.root #this creates a table in the window
 
 root.mainloop() #this keeps the window open
